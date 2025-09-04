773731e4ec664c6cd8e567955091974e9622aa4e
Lev <lev01111989@gmail.com>
2025-09-04 07:49:57 +0200

    Atempt2

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,19 @@
-urlpatterns = []
+from os import path
+
+from musician.views import (MusicianListCreateView,
+                            MusicianRetrieveUpdateDestroyView)
+
+urlpatterns = [
+    path(
+        "musicians/",
+        MusicianListCreateView.as_view(),
+        name="manage-list"
+    ),
+    path(
+        "musicians/<int:pk>/",
+        MusicianRetrieveUpdateDestroyView.as_view(),
+        name="musician-detail"
+    ),
+]
 
 app_name = "musician"
